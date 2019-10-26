d35bad6a45fd7c3a08cacbb81beab1d13fa7b95e
David Sergio <davidsergio52@gmail.com>
2019-10-26 13:06:21 -0700

    updated nullable and config classes

---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    class EnvironmentConfig : IConfig
+    {
+        public bool GetConfigValue(string name, out string? value)
+        {
+            value = Environment.GetEnvironmentVariable(name);
+            return !(value is null);
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
