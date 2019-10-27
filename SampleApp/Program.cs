cd63b32216c68cc35bf0283095811e0cc17fdd53
David Sergio <davidsergio52@gmail.com>
2019-10-26 22:22:49 -0700

    updates

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Configuration;
+using System;
+using System.Collections.Generic;
 
 namespace SampleApp
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // print out all the configuration settings based on hard coded values for config
+            List<string> hardcodedConfigNames = new List<string>
+            {
+                "PROCESSOR_ARCHITECTURE",
+                "USERNAME",
+                "HOMEPATH"
+            };
+
+            IConfig environmentConfig = new EnvironmentConfig();
+
+            foreach (string s in hardcodedConfigNames)
+            {
+                environmentConfig.GetConfigValue(s, out string? val);
+                Console.WriteLine(s + "=" + val);
+            }
+
+            //environmentConfig.GetConfigValue("COMPUTERNAME", out string str);
+            //Console.WriteLine(str);
+
+            //environmentConfig.SetConfigValue("somename", "someval");
+            //environmentConfig.GetConfigValue("somename", out string? str);
+            //Console.WriteLine("str: " + str);
+
+            //IConfig fileConfig = new FileConfig();
+            //fileConfig.SetConfigValue("name", "myval");
         }
     }
 }
