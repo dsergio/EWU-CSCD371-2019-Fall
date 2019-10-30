107f47eee1f889acc77cd9fb3ece1a95e26654df
David Sergio <davidsergio52@gmail.com>
2019-10-29 23:29:11 -0700

    updated size

--- Mailbox/Size.cs
+++ Mailbox/Size.cs
@@ -1,4 +1,14 @@
-﻿namespace Mailbox
-{
+﻿
+using System;
 
+namespace Mailbox
+{
+    [Flags]
+    public enum Size
+    {
+        Medium = 0b0000,
+        Small = 0b0001,
+        Large = 0b0010,
+        Premium = 0b0100
+    }
 }
