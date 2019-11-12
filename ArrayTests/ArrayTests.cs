eb26f9915672a182d08549d4cf7957f8be4ecb97
David Sergio <davidsergio52@gmail.com>
2019-11-11 17:30:24 -0800

    extra credit, updated tests and global suppressions

--- ArrayTests/ArrayTests.cs
+++ ArrayTests/ArrayTests.cs
@@ -1,5 +1,10 @@
 using Assignment6;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace ArrayTests
 {
@@ -7,58 +12,41 @@ namespace ArrayTests
     public class ArrayTests
     {
         [TestMethod]
-        public void Array_SetGetInBounds_ReturnsCorrectValue()
+        public void ArrayString_SetGetInBounds_ReturnsCorrectValue()
         {
             // Arrange
             ArrayCollection<string> arr = new ArrayCollection<string>(10);
 
             // Act
-            arr.Set(0, "hi");
-            bool ret = arr.TryGetValue(0, out string value);
+            arr.Add("hi");
+            string value = arr[0];
 
             // Assert
             Assert.AreEqual<string>("hi", value);
-            Assert.IsTrue(ret);
         }
 
         [TestMethod]
-        public void Array_SetInBoundsGetOutBounds_ReturnsFalseAndNull()
+        public void ArrayInt_SetGetInBounds_ReturnsCorrectValue()
         {
             // Arrange
-            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+            ArrayCollection<int> arr = new ArrayCollection<int>(10);
 
             // Act
-            arr.Set(0, "hi");
-            bool ret = arr.TryGetValue(20, out string value);
+            arr.Add(3);
+            int value = arr[0];
 
             // Assert
-            Assert.IsFalse(ret);
-            Assert.IsNull(value);
+            Assert.AreEqual<int>(3, value);
         }
 
         [TestMethod]
-        public void Array_SetGetOutBounds_ReturnsFalseAndNull()
+        public void ArrayString_Contains_ReturnsFalse()
         {
             // Arrange
             ArrayCollection<string> arr = new ArrayCollection<string>(10);
 
             // Act
-            arr.Set(15, "hi");
-            bool ret = arr.TryGetValue(15, out string value);
-
-            // Assert
-            Assert.IsFalse(ret);
-            Assert.IsNull(value);
-        }
-
-        [TestMethod]
-        public void Array_Contains_ReturnsFalse()
-        {
-            // Arrange
-            ArrayCollection<string> arr = new ArrayCollection<string>(10);
-
-            // Act
-            arr.Set(5, "hi");
+            arr.Add("hi");
             bool ret = arr.Contains("ho");
 
             // Assert
@@ -66,13 +54,13 @@ namespace ArrayTests
         }
 
         [TestMethod]
-        public void Array_Contains_ReturnsTrue()
+        public void ArrayString_Contains_ReturnsTrue()
         {
             // Arrange
             ArrayCollection<string> arr = new ArrayCollection<string>(10);
 
             // Act
-            arr.Set(5, "hi");
+            arr.Add("hi");
             bool ret = arr.Contains("hi");
 
             // Assert
@@ -80,33 +68,377 @@ namespace ArrayTests
         }
 
         [TestMethod]
-        public void Array_IndexOperatorInBounds_ReturnsCorrectValue()
+        public void ArrayString_IndexOperatorInBounds_ReturnsCorrectValue()
         {
             // Arrange
             ArrayCollection<string> arr = new ArrayCollection<string>(10);
 
             // Act
-            arr[5] = "hi";
-            bool ret = arr.Contains("hi");
+            arr.Add("zero");
+            arr.Add("one");
+            arr.Add("two");
+            arr.Add("three");
+            arr.Add("four");
+            arr.Add("five");
+            bool ret = arr.Contains("five");
 
             // Assert
             Assert.IsTrue(ret);
-            Assert.AreEqual<string>("hi", arr[5]);
+            Assert.AreEqual<string>("five", arr[5]);
         }
 
         [TestMethod]
-        public void Array_IndexOperatorOutBounds_ReturnsCorrectValue()
+        [ExpectedException(typeof(ArgumentException))]
+        [ExcludeFromCodeCoverage]
+        public void ArrayString_IndexOperatorOutBounds_ThrowsException()
         {
             // Arrange
             ArrayCollection<string> arr = new ArrayCollection<string>(10);
 
             // Act
-            arr[15] = "hi";
-            bool ret = arr.Contains("hi");
+            arr.Add("zero");
+            arr.Add("one");
+            arr.Add("two");
+            arr.Add("three");
+            arr.Add("four");
+            arr.Add("five");
+            string value = arr[15];
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void ArrayString_Foreach_CorrectValue()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("zero_");
+            arr.Add("one_");
+            arr.Add("two_");
+            arr.Add("three_");
+            arr.Add("four_");
+            arr.Add("five_");
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in arr)
+            {
+                Assert.IsTrue(s is string);
+                sb.Append(s);
+            }
+
+            // Assert
+            Assert.AreEqual<string>("zero_one_two_three_four_five_", sb.ToString());
+        }
+
+        [TestMethod]
+        public void ArrayString_AddItems_CountIsCorrect()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("one");
+            arr.Add("two");
+
+            // Assert
+            Assert.AreEqual<int>(2, arr.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [ExcludeFromCodeCoverage]
+        public void ArrayString_AddNullItem_ThrowsException()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add(null);
+            
+
+            // Assert
+            
+        }
+
+        [TestMethod]
+        public void ArrayString_ClearArray_CountIsZero()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("hi");
+            arr.Clear();
+
+            // Assert
+            Assert.AreEqual<int>(0, arr.Count);
+        }
+
+        [TestMethod]
+        public void ArrayString_RemoveItem_ContentsAreCorrect()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("hi");
+            arr.Add("ho");
+            arr.Remove("hi");
+
+            // Assert
+            Assert.AreEqual<int>(1, arr.Count);
+            Assert.IsFalse(arr.Contains("hi"));
+            Assert.IsTrue(arr.Contains("ho"));
+        }
+
+        [TestMethod]
+        public void ArrayString_SetReadOnlyTrue_RemoveReturnsFalse()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("hi");
+            arr.Add("ho");
+            arr.IsReadOnly = true;
+            bool ret = arr.Remove("hi");
 
             // Assert
             Assert.IsFalse(ret);
-            Assert.AreNotEqual<string>("hi", arr[15]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        [ExcludeFromCodeCoverage]
+        public void ArrayString_RemoveItemDoesntExist_ThrowsException()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            bool ret = arr.Remove("hi");
+
+            // Assert
+            Assert.IsFalse(ret);
+        }
+
+        [TestMethod]
+        public void ArrayString_SetReadOnlyTrue_AddDoesntChange()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("hi");
+            arr.Add("ho");
+            arr.IsReadOnly = true;
+            arr.Add("hum");
+
+            // Assert
+            Assert.IsFalse(arr.Contains("hum"));
+        }
+
+        [TestMethod]
+        public void ArrayString_CopyTo_CorrectContents()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("hi");
+            arr.Add("ho");
+            string[] strArr = new string[10];
+
+            arr.CopyTo(strArr, arr.Count);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in strArr) 
+            {
+                sb.Append(s);
+            }
+
+            // Assert
+            Assert.AreEqual<string>("hiho", sb.ToString());
+        }
+
+        [TestMethod]
+        public void ArrayString_CreateReadOnlyArray_CannotChange()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10, true);
+
+            // Act
+            arr.Add("hi");
+            arr.Add("ho");
+            
+
+            // Assert
+            Assert.IsFalse(arr.Contains("hi"));
+            Assert.IsFalse(arr.Contains("ho"));
+            Assert.AreEqual<int>(0, arr.Count);
+        }
+
+        [TestMethod]
+        public void ArrayString_GetEnumerator_CorrectValues()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("hi");
+            arr.Add("ho");
+            IEnumerator<string> e = arr.GetEnumerator();
+            
+            StringBuilder sb = new StringBuilder();
+
+            while (e.MoveNext())
+            {
+                sb.Append(e.Current);
+            }
+
+            // Assert
+            Assert.AreEqual<string>("hiho", sb.ToString());
+        }
+
+        [TestMethod]
+        public void ArrayString_SetItemValid_ReturnsCorrectValue()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("hi");
+            arr.Add("ho");
+            arr[0] = "ho";
+
+            // Assert
+            Assert.AreEqual<string>("ho", arr[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [ExcludeFromCodeCoverage]
+        public void ArrayString_SetItemInvalid_ThrowsException()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+
+            // Act
+            arr.Add("hi");
+            arr.Add("ho");
+            arr[0] = null;
+
+            // Assert
+            
+        }
+
+        [DataTestMethod]
+        [DataRow(new string[] { "one", "two", "three"})]
+        [DataRow(new string[] { "", "two", "" })]
+        public void ArrayString_CollectionInitializer_CorrectContents(string[] strArr)
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(strArr);
+
+            // Act
+            string s = arr[1];
+
+            // Assert
+            Assert.AreEqual<string>("two", s);
+            Assert.AreEqual(3, arr.Count);
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [ExcludeFromCodeCoverage]
+        public void ArrayString_CollectionInitializerNull_ThrowsException(string[] strArr)
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(strArr);
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(0, arr.Count);
+        }
+
+        /// <summary>
+        /// Extra Credit
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void ArrayString_CastArray_CorrectValues()
+        {
+            // Arrange
+            ArrayCollection<string> arr = new ArrayCollection<string>(10);
+            arr.Add("hi");
+            arr.Add("ho");
+
+            // Act
+            string[] strArr = (string[])arr;
+
+            // Assert
+            Assert.AreEqual<string>("hi", strArr[0]);
+            Assert.AreEqual<string>("ho", strArr[1]);
+        }
+
+        /// <summary>
+        /// Extra Credit
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void ArrayString_CastArrayCollection_CorrectValues()
+        {
+            // Arrange
+            string[] strArr = new string[] { "hi", "ho" };
+
+            // Act
+            ArrayCollection<string> arr = (ArrayCollection<string>)strArr;
+
+            // Assert
+            Assert.AreEqual<string>("hi", arr[0]);
+            Assert.AreEqual<string>("ho", arr[1]);
+        }
+
+        /// <summary>
+        /// Extra Credit
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void ArrayInt_CastArray_CorrectValues()
+        {
+            // Arrange
+            ArrayCollection<int> arr = new ArrayCollection<int>(10);
+            arr.Add(4);
+            arr.Add(5);
+
+            // Act
+            int[] intArr = (int[])arr;
+
+            // Assert
+            Assert.AreEqual<int>(4, intArr[0]);
+            Assert.AreEqual<int>(5, intArr[1]);
+        }
+
+        /// <summary>
+        /// Extra Credit
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void ArrayInt_CastArrayCollection_CorrectValues()
+        {
+            // Arrange
+            int[] intArr = new int[] { 8, 9 };
+
+            // Act
+            ArrayCollection<int> arr = (ArrayCollection<int>)intArr;
+
+            // Assert
+            Assert.AreEqual<int>(8, arr[0]);
+            Assert.AreEqual<int>(9, arr[1]);
         }
     }
 }
--- Assignment6/ArrayCollection.cs
+++ Assignment6/ArrayCollection.cs
@@ -4,19 +4,33 @@ using System.Collections.Generic;
 
 namespace Assignment6
 {
-    public class ArrayCollection<T> : System.Collections.Generic.ICollection<T> where T: class
+    public class ArrayCollection<T> : ICollection<T> // where T: class
     {
         private List<T> Data { get; set; }
         public int Capacity { get; private set; }
-
-        public int Count { get; private set; }
+        public int Count => Data.Count;
 
         public bool IsReadOnly { get; set; }
+
+        
 
         public ArrayCollection(int capacity)
         {
             Data = new List<T>();
             Capacity = capacity;
+            IsReadOnly = false;
+
+        }
+
+        public ArrayCollection(ICollection<T> collection)
+        {
+            if (collection is null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            Data = new List<T>(collection);
+            Capacity = Data.Capacity;
             IsReadOnly = false;
         }
 
@@ -38,36 +52,15 @@ namespace Assignment6
                 return Data[i];
             }
             set {
-                if (!IsReadOnly)
+                if (value is null)
                 {
-                    Set(i, value);
+                    throw new ArgumentNullException(nameof(value));
+                }
+                if (!IsReadOnly && i < Count)
+                {
+                    Data[i] = value;
                 }
             }
-        }
-
-        public bool TryGetValue(int i, out T? value)
-        {
-            if (Data[i] is T)
-            {
-                value = Data[i];
-                return true;
-            } else
-            {
-                value = default;
-                return false;
-            }
-            //bool ret = Data.TryGetValue(i, out T internalValue);
-            //value = internalValue;
-            //return ret;
-        }
-
-        public void Set(int i, T value)
-        {
-            if (i >= Capacity || i < 0)
-            {
-                throw new ArgumentException("Index out of range.", nameof(i));
-            }
-            Data[i] = value;
         }
 
         public bool Contains(T value)
@@ -75,18 +68,25 @@ namespace Assignment6
             return Data.Contains(value);
         }
 
-        public void Add(T? item)
+        public void Add(T item)
         {
             if (item is null)
             {
                 throw new ArgumentNullException(nameof(item));
             }
-            Data.Add(item);
+            if (!IsReadOnly)
+            {
+                Data.Add(item);
+            }
+            
         }
 
         public void Clear()
         {
-            Data.Clear();
+            if (!IsReadOnly)
+            {
+                Data.Clear();
+            }
         }
 
         public void CopyTo(T[] array, int arrayIndex)
@@ -100,7 +100,14 @@ namespace Assignment6
             {
                 throw new ArgumentException("Item does not exist", nameof(item));
             }
-            return Data.Remove(item);
+            if (!IsReadOnly)
+            {
+                return Data.Remove(item);
+            } else
+            {
+                return false;
+            }
+            
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -111,6 +118,47 @@ namespace Assignment6
         IEnumerator IEnumerable.GetEnumerator()
         {
             return Data.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Extra Credit
+        /// 
+        /// </summary>
+        /// <param name="arr"></param>
+        public static explicit operator Array(ArrayCollection<T> arr)
+        {
+            if (arr is null)
+            {
+                throw new ArgumentNullException(nameof(arr));
+            }
+            T[] ret = new T[arr.Count];
+            int i = 0;
+            foreach (T t in arr)
+            {
+                ret[i++] = t;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Extra Credit
+        /// 
+        /// </summary>
+        /// <param name="arr"></param>
+        public static explicit operator ArrayCollection<T>(Array arr)
+        {
+            if (arr is null)
+            {
+                throw new ArgumentNullException(nameof(arr));
+            }
+            ArrayCollection<T> ret = new ArrayCollection<T>(arr.Length);
+            foreach (T t in arr)
+            {
+                ret.Add(t);
+            }
+
+            return ret;
         }
     }
 }
