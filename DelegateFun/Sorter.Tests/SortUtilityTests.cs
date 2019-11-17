2aa6aa61012a3833424bec94fe491f7fdedae883
David Sergio <davidsergio52@gmail.com>
2019-11-16 23:35:13 -0800

    hw 7 solution wip

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using static Sorter.SortUtility;
 
 namespace Sorter.Tests
 {
@@ -8,6 +11,41 @@ namespace Sorter.Tests
         [TestMethod]
         public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
         {
+            int length = 5;
+            int[] arr = new int[length];
+            Random r = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = r.Next(10);
+            }
+
+            compareFunction greaterThan = delegate (int i, int j) {
+                return i > j;
+            };
+            compareFunction lessThan = delegate (int i, int j) {
+                return i < j;
+            };
+
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+
+            Trace.WriteLine("");
+            Trace.WriteLine("Unsorted: ");
+            for (int i = 0; i < length; i++)
+            {
+                Trace.Write(arr[i] + " ");
+            }
+            Trace.WriteLine("");
+            Trace.WriteLine("Sorted: ");
+            SortUtility.Sort(arr, lessThan);
+            for (int i = 0; i < length; i++)
+            {
+                Trace.Write(arr[i] + " ");
+            }
+            Trace.WriteLine("");
+            Assert.IsTrue(true);
+
+
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -6,5 +6,51 @@ namespace Sorter
     {
         // Sort method should be implemented here
         // It should accept an int[] and a delegate you define that performs the actual comparison
+
+        public delegate bool compareFunction(int i, int j);
+
+        public static void Sort(int[] arr, compareFunction compare)
+        {
+            QuickSort(arr, 0, arr.Length - 1, compare);
+        }
+
+        private static void QuickSort(int[] arr, int low, int high, compareFunction compare)
+        {
+            int pivot = arr[low + (high - low) / 2];
+            Console.WriteLine("pivot: " + pivot + " low: " + low + " high: " + high);
+
+            int i = low;
+            int j = high;
+
+            while (i <= j)
+            {
+                while (compare(arr[i], pivot))
+                {
+                    i++;
+                }
+                while (compare(pivot, arr[j]))
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (low < j)
+            {
+                QuickSort(arr, low, j, compare);
+            }
+            if (i < high)
+            {
+                QuickSort(arr, i, high, compare);
+            }
+        }
+
     }
 }
