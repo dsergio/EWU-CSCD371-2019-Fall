eea355387dd95dbbed44261cb1c61cf3ef1e4154
David Sergio <davidsergio52@gmail.com>
2019-11-17 20:27:15 -0800

    updated tests

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -8,44 +8,72 @@ namespace Sorter.Tests
     [TestClass]
     public class SortUtilityTests
     {
-        [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        private int[] getRandomArray(int length)
         {
-            int length = 5;
             int[] arr = new int[length];
             Random r = new Random();
             for (int i = 0; i < length; i++)
             {
                 arr[i] = r.Next(10);
             }
+            return arr;
+        }
 
-            compareFunction greaterThan = delegate (int i, int j) {
-                return i > j;
-            };
-            compareFunction lessThan = delegate (int i, int j) {
-                return i < j;
-            };
+        compareFunction greaterThan = delegate (int i, int j) {
+            return i > j;
+        };
+        compareFunction lessThan = delegate (int i, int j) {
+            return i < j;
+        };
+
+        [TestMethod]
+        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        {
+            // Arrange
+            int[] arr = getRandomArray(10);
 
             Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
-
-
             Trace.WriteLine("");
             Trace.WriteLine("Unsorted: ");
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
                 Trace.Write(arr[i] + " ");
             }
             Trace.WriteLine("");
             Trace.WriteLine("Sorted: ");
+
+            // Act
             SortUtility.Sort(arr, lessThan);
-            for (int i = 0; i < length; i++)
+
+            // Assert
+            for (int i = 0; i < arr.Length; i++)
             {
                 Trace.Write(arr[i] + " ");
             }
             Trace.WriteLine("");
             Assert.IsTrue(true);
+        }
 
+        [TestMethod]
+        public void SortUtility_ShouldSortAscending_UsingLambdaExpression()
+        {
+            // Arrange
+            int[] arr = getRandomArray(10);
 
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void SortUtility_ShouldSortAscending_UsingLambdaStatement()
+        {
+            // Arrange
+            int[] arr = getRandomArray(10);
+
+            // Act
+
+            // Assert
         }
     }
 }
