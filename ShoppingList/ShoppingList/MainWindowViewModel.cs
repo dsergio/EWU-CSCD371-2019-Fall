9c22eb4db9ad7293241218100eb39992fc287e2b
David Sergio <davidsergio52@gmail.com>
2019-12-07 20:38:00 -0800

    assignment 9

---
+++ ShoppingList/ShoppingList/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace ShoppingList
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ObservableCollection<ShoppingItem> ShoppingItemList { get; } = new ObservableCollection<ShoppingItem>();
+
+        public Command AddItemCommand { get; }
+
+        private bool CanExecute { get; set; }
+
+        private ShoppingItem _SelectedShoppingItem;
+        public ShoppingItem SelectedShoppingItem
+        {
+            get => _SelectedShoppingItem;
+            set => SetProperty(ref _SelectedShoppingItem, value);
+        }
+
+        public enum UnitType
+        {
+            Gallon,
+            FluidOunce,
+            Liter,
+            Gram,
+            Pound,
+            Each
+        }
+        public MainWindowViewModel()
+        {
+            ShoppingItemList.Add(new ShoppingItem("Half & half", true, 1, 0.5, UnitType.Gallon));
+            ShoppingItemList.Add(new ShoppingItem("Coffee", true, 1, 1, UnitType.Pound));
+            ShoppingItemList.Add(new ShoppingItem("Bananas", true, 5, 1, UnitType.Each));
+            SelectedShoppingItem = ShoppingItemList.First();
+            AddItemCommand = new Command(OnAddItem, () => CanExecute);
+        }
+
+        private void OnAddItem()
+        {
+            ShoppingItemList.Add(new ShoppingItem("Bananas", true, 5, 1, UnitType.Each));
+            CanExecute = true;
+            AddItemCommand.InvokeCanExecuteChanged();
+        }
+    }
+}
---
+++ ShoppingList/ShoppingList/ShoppingItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ShoppingList.MainWindowViewModel;
+
+namespace ShoppingList
+{
+    public class ShoppingItem
+    {
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public int Quantity { get; set; }
+        public double Size { get; set; }
+        public UnitType Unit { get; set; }
+
+        public ShoppingItem(string name, bool active, int quantity, double size, UnitType unit)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Active = active;
+            Quantity = quantity;
+            Size = size;
+            Unit = unit;
+        }
+    }
+}
